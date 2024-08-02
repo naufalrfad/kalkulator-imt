7c03b1beb77b285251119371cf2e38977dac4b34
naufalrfad <naufalrizki728@gmail.com>
2024-08-02 13:35:20 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,24 +5,22 @@ function calculateIMT() {
 
     var classification = '';
     if (imt < 18.5) {
-        classification = 'Berat badan kurang';
-    } else if (imt >= 18.5 && imt <= 24.9) {
+        classification = 'Berat badan kurang (Underweight)';
+    } else if (imt >= 18.5 && imt <= 22.9) {
         classification = 'Normal';
+    } else if (imt >= 23 && imt <= 24.9) {
+        classification = 'Kelebihan berat badan (Overweight)';
     } else if (imt >= 25 && imt <= 29.9) {
-        classification = 'Kelebihan berat badan';
-    } else if (imt >= 30 && imt <= 34.9) {
         classification = 'Obesitas I';
-    } else if (imt >= 35 && imt <= 39.9) {
+    } else if (imt >= 30) {
         classification = 'Obesitas II';
-    } else if (imt >= 40) {
-        classification = 'Obesitas III';
     }
 
     document.getElementById('classification').innerText = classification;
     document.getElementById('imtResult').innerText = imt;
 
     var pointer = document.getElementById('pointer');
-    var percentage = ((imt - 10) * 5).toFixed(1);
+    var percentage = ((imt - 10) * 2).toFixed(1);
     if (percentage < 0) percentage = 0;
     if (percentage > 100) percentage = 100;
     pointer.style.left = `calc(${percentage}% - 5px)`;
